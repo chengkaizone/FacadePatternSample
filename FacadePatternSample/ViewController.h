bb4bb82b3b0c1482eb110314cd974f5cb1288d6b
chengkaizone <chengkaizone@163.com>
2015-10-10 21:51:23 +0800

    Initial Commit

---
+++ FacadePatternSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FacadePatternSample
+//
+//  Created by lance on 15/10/10.
+//  Copyright © 2015年 lance. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
